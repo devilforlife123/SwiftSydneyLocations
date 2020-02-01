0fc2c5d4d70aa49eb73b363ba40907fa6cb8226f
devilforlife123 <surajpoudelg@gmail.com>
2020-02-01 12:13:18 +1100

    Initial Commit

---
+++ SwiftSyndeyLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSyndeyLocations
+//
+//  Created by suraj poudel on 1/2/20.
+//  Copyright © 2020 suraj poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
